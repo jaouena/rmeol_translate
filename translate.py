6ffde08b381a451d1f7f3fed7f62d3da09c73090
jaouena <jaouenadel@gmail.com>
2019-11-18 10:30:52 +0100

    translate.py

---
+++ translate.py
@@ -0,0 +1,22 @@
+from googletrans import Translator
+import argparse
+
+parser = argparse.ArgumentParser(description='translate a text in french with googletrans api')
+parser.add_argument('--textfile', type=str,
+                    help='a .txt file to translate')
+                    
+                    
+
+file = parser.parse_args().textfile
+
+
+def __main__():
+    translator = Translator()
+    with open(file) as f:
+        list_of_lines = f.read().splitlines()
+        text = ' '.join(list_of_lines)
+        translated = translator.translate(text, dest='fr')
+        open('translated_%s'%file, 'w').write(translated.text)
+    
+if __name__=='__main__':
+    __main__()
