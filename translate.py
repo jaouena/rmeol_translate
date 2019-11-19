b48d0909b35e341f0e79907bc79b76fdc4e66159
jaouena <jaouenadel@gmail.com>
2019-11-19 12:13:09 +0100

    remove '-' in end line before translate

--- translate.py
+++ translate.py
@@ -14,6 +14,9 @@ def __main__():
     translator = Translator()
     with open(file) as f:
         list_of_lines = f.read().splitlines()
+        for i, line in enumerate(list_of_lines):
+            if line.endswith('-'):
+                list_of_lines[i] = line[:-1]
         text = ' '.join(list_of_lines)
         translated = translator.translate(text, dest='fr')
         open('translated_%s'%file, 'w').write(translated.text)
